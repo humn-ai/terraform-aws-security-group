c1fff34a2e1a79605c29c4b8e97bf0432a13d8a1
Callum <callumccr@gmail.com>
2021-04-12 14:48:10 +0100

    updated variables

--- variables.tf
+++ variables.tf
@@ -1,19 +1,35 @@
+# -----------------------------------------------------------------------------
+# Variables: Common AWS Provider - Autoloaded from Terragrunt
+# -----------------------------------------------------------------------------
+
+variable "aws_region" {
+  description = "The AWS region (e.g. ap-southeast-2). Autoloaded from region.tfvars."
+  type        = string
+  default     = ""
+}
+
+variable "aws_account_id" {
+  description = "The AWS account id of the provider being deployed to (e.g. 12345678). Autoloaded from account.tfvars"
+  type        = string
+  default     = ""
+}
+
+variable "aws_assume_role_arn" {
+  description = "(Optional) - ARN of the IAM role when optionally connecting to AWS via assumed role. Autoloaded from account.tfvars."
+  type        = string
+  default     = ""
+}
+
+# -----------------------------------------------------------------------------
+# Variables: TF-MOD-AWS-SECURITY-GROUP
+# -----------------------------------------------------------------------------
+
 #################
 # Security group
 #################
-variable "create" {
-  description = "Whether to create security group and all rules"
-  type        = bool
-  default     = true
-}
 
 variable "vpc_id" {
   description = "ID of the VPC where to create security group"
-  type        = string
-}
-
-variable "name" {
-  description = "Name of security group"
   type        = string
 }
 
